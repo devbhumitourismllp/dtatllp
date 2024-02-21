90474a470080fccc817a255ad94a3844162a76cc
Lalit Mohan <158826213+devbhumitourismllp@users.noreply.github.com>
2024-02-21 22:24:19 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -126,12 +126,12 @@ document.addEventListener("DOMContentLoaded", () => {
     breakpoints: {
       320: {
         slidesPerView: 1,
-        spaceBetween: 10,
+        spaceBetween: 3,
       },
 
       1200: {
         slidesPerView: 5,
-        spaceBetween: 10,
+        spaceBetween: 3,
       },
     },
   });
